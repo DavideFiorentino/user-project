d4fd654ef5c3306a774a16fe858b184375699210
Davide Fiorentino <davide.fiorentino01@gmail.com>
2021-10-16 15:21:54 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'user-prj-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'user-prj';
+}
